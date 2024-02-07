9d23f54d61f55a8f60b70a7d108b7faf1e8b7d0d
eljohnnyy <you@example.com>
2024-02-07 22:56:52 +0200

    create main page and main page body

---
+++ lib/Features/home/presentation/views/widgets/main_page_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainPageBody extends StatelessWidget {
+  const MainPageBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
